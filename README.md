ac20e639230841cd23ef17ef7b1f2f55a5e91b2b
Mark Nottingham <mnot@mnot.net>
2020-10-27 13:19:28 +1100

    external configuration

--- README.md
+++ README.md
@@ -2,23 +2,53 @@
 
 This is a simple script to validate [Structured Fields](https://httpwg.org/http-extensions/draft-ietf-httpbis-header-structure.html) in [xml2rfcv3](https://tools.ietf.org/html/rfc7991) documents.
 
-It examines all `sourcecode` and `artwork` elements; when one has a `type` of `http-sf-item`, `http-sf-list` or `http-sf-dict`, it validates the contents.
+## Structured Fields in RFC XML
 
-When validating, it assumes that the content is a HTTP field section; that is, one or more lines, each in the format `field_name: field_value`. Line folding is supported, so that long lines can be formatted appropriately. Multiple lines with the same name will be combined into one value. Then, each value is parsed as the Structured Field type indicated by the `type` attribute.
+This script examines all `sourcecode` and `artwork` elements; when one has a `type` of
+`http-structured-fields`, it assumes that the content is a HTTP field section; that is, one or more
+lines, each in the format `field_name: field_value`. Line folding is supported, so that long lines
+can be formatted appropriately. Multiple lines with the same name will be combined into one value.
+
+Then, each value is parsed as a Structured Field type, if the header name is recognised as being a structured field.
 
 For example,
 
 ~~~ xml
-<sourcecode type="http-sf-list">
+<sourcecode type="http-structured-fields">
 Foo: bar; baz
 Foo: one,
      two
 </sourcecode>
 ~~~
 
-... will be validated as a Structured Field List with the value `bar; baz, one, two`.
+... will be validated as having a single field, `foo`, with the value `bar; baz, one, two`.
 
 Note that in your XML, there **must not be any whitespace** at the start of lines, unless they're continuation of previous lines (folding, as seen above).
+
+
+## Configuring Structured Type Information
+
+You can pass type information (i.e., List, Dictionary, or Item) for field names on the command line, or in a configuration file.
+
+To pass a type on the command line, use the `--list`, `--dictionary` or `--item` arguments as appropriate, followed by the field name. For example:
+
+> sf-rfc-validate.py --list Foo --list Bar --item Baz my_draft_.xml
+
+Here, `Foo` and `Bar` will be validated as Structured Lists, while `Baz` will be validated as a Structured Item.
+
+Alternatively, you can collect this information in a JSON file, with the top-level object keys being field names, and their values being `list`, `dict` or `item` as appropriate. Thus, the configuration in the example above could be expressed in a JSON file `sf.json` as:
+
+~~~ json
+{
+  "Foo": "list",
+  "Bar": "list",
+  "Baz": "item"
+}
+~~~
+
+... and passed to the script like this:
+
+> sf-rfc-validate.py --map sf.json my_draft.xml
 
 
 ## Installation
@@ -27,9 +57,3 @@ The script requires Python 3, and can be installed with pip:
 
 > pip3 install sf-rfc-validate
 
-
-## Use
-
-> sf-rfc-validate.py rfcNNNN.xml
-
-If there are errors, they will be listed, and the program will return 1.
--- sf-rfc-validate.py
+++ sf-rfc-validate.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
 
+import argparse
+import json
 import sys
 from xml import sax
 
 import http_sfv
 
-typeMap = {
-    "http-sf-item": http_sfv.Item,
-    "http-sf-list": http_sfv.List,
-    "http-sf-dict": http_sfv.Dictionary,
-    "http-sf-invalid": None,
-}
 
-
-def validate(filenames):
+def validate(filehandles, typemap):
     errors = 0
-    for filename in filenames:
-        print(f"* Validating {filename}")
-        handler = SfValidator()
-        sax.parse(filename, handler)
+    for fh in filehandles:
+        print(f"* Validating {fh.name}")
+        handler = SfValidator(typemap)
+        sax.parse(fh, handler)
         errors += handler.errors
         print()
     if errors > 0:
@@ -26,8 +21,9 @@ def validate(filenames):
 
 
 class SfValidator(sax.ContentHandler):
-    def __init__(self, *args, **kw):
-        sax.ContentHandler.__init__(self, *args, **kw)
+    def __init__(self, typemap):
+        sax.ContentHandler.__init__(self)
+        self.typemap = typemap
         self.listening = False
         self.content = ""
         self.type = None
@@ -40,23 +36,26 @@ class SfValidator(sax.ContentHandler):
 
     def endElement(self, name):
         if self.listening:
-            header_type = typeMap.get(self.type)
-            if header_type:
+            if self.type in ["http-structured-headers"]:
                 try:
                     headers = combine_headers(self.content)
                 except ValueError as why:
-                    print(f"* ERROR - {why}")
+                    print(f"  ERROR - {why}")
                     self.errors += 1
                     return
                 for hname, hvalue in headers.items():
-                    try:
-                        print(f"  checking {hname}: {hvalue}")
-                        header_type().parse(hvalue.encode("ascii"))
-                    except ValueError as why:
-                        print(f"* ERROR - {why}")
-                        self.errors += 1
+                    header_type = self.typemap.get(hname)
+                    if header_type:
+                        try:
+                            print(f"  validating {hname}: {hvalue}")
+                            header_type().parse(hvalue.encode("ascii"))
+                        except ValueError as why:
+                            print(f"  ERROR - {why}")
+                            self.errors += 1
+                    else:
+                        print(f"  skipping {hname} field (no type information)")
             else:
-                print(f"  skipping {self.type}")
+                print(f"  skipping {self.type} section")
             self.listening = False
             self.content = ""
 
@@ -93,5 +92,76 @@ def combine_headers(content):
     return headers
 
 
+def main():
+    parser = argparse.ArgumentParser(
+        description="Validate HTTP Structured Fields in XML2RFC documents"
+    )
+    parser.add_argument(
+        "-m",
+        "--map",
+        dest="map",
+        type=argparse.FileType("r"),
+        help="JSON file that maps field names to structured types",
+    )
+    parser.add_argument(
+        "-i",
+        "--item",
+        dest="item",
+        action="append",
+        default=[],
+        help="field name to consider as a Structured Item",
+    )
+    parser.add_argument(
+        "-l",
+        "--list",
+        dest="list",
+        action="append",
+        default=[],
+        help="field name to consider as a Structured List",
+    )
+    parser.add_argument(
+        "-d",
+        "--dict",
+        dest="dict",
+        action="append",
+        default=[],
+        help="field name to consider as a Structured Dictionary",
+    )
+    parser.add_argument(
+        "file", type=argparse.FileType("r"), nargs="+", help="an XML file to validate"
+    )
+
+    args = parser.parse_args()
+    typemap = {}
+    if args.map:
+        try:
+            rawmap = json.load(args.map)
+        except (IOError, ValueError) as why:
+            sys.stderr.write(f"ERROR loading JSON: {why}\n")
+            sys.exit(1)
+        try:
+            typemap.update(
+                {
+                    k.lower(): {
+                        "item": http_sfv.Item,
+                        "list": http_sfv.List,
+                        "dict": http_sfv.Dictionary,
+                    }[v]
+                    for (k, v) in rawmap.items()
+                }
+            )
+        except KeyError as why:
+            sys.stderr.write(f"ERROR loading JSON value: {why}\n")
+            sys.exit(1)
+    for item in args.item:
+        typemap[item.lower()] = http_sfv.Item
+    for _list in args.list:
+        typemap[_list.lower()] = http_sfv.List
+    for _dict in args.dict:
+        typemap[_dict.lower()] = http_sfv.Dictionary
+
+    validate(args.file, typemap)
+
+
 if __name__ == "__main__":
-    validate(sys.argv[1:])
+    main()
