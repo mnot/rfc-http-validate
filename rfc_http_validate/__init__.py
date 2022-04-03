0620fdcbfa9a07aba8b45aea6fb3c0ed5d546956
Mark Nottingham <mnot@mnot.net>
2022-04-04 08:37:21 +1000

    Move CLI to an object

--- rfc_http_validate/__init__.py
+++ rfc_http_validate/__init__.py
@@ -10,105 +10,108 @@ import http_sfv
 from .validate import RfcHttpValidator
 
 __version__ = "0.1.5"
-verbose = True
 
 
-def status(*args):
-    global verbose
-    if verbose:
-        print(*args)
+class ValidatorCLI:
+    def __init__(self):
+        self.args = self.parse_args()
+        self.typemap = self.load_typemap()
+        self.run()
 
+    def status(self, *args):
+        if not self.args.quiet:
+            print(*args)
 
-def validate(filehandles, typemap):
-    errors = 0
-    for fh in filehandles:
-        handler = RfcHttpValidator(typemap, status)
-        handler.filename = fh.name
-        sax.parse(fh, handler)
-        errors += handler.errors
-    if errors > 0:
-        sys.exit(1)
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description="Validate HTTP messages in XML2RFC documents"
-    )
-    parser.add_argument(
-        "-m",
-        "--map",
-        dest="map",
-        type=argparse.FileType("r"),
-        help="JSON file that maps field names to structured types",
-    )
-    parser.add_argument(
-        "-i",
-        "--item",
-        dest="item",
-        action="append",
-        default=[],
-        help="field name to consider as a Structured Item",
-    )
-    parser.add_argument(
-        "-l",
-        "--list",
-        dest="list",
-        action="append",
-        default=[],
-        help="field name to consider as a Structured List",
-    )
-    parser.add_argument(
-        "-d",
-        "--dict",
-        dest="dict",
-        action="append",
-        default=[],
-        help="field name to consider as a Structured Dictionary",
-    )
-    parser.add_argument(
-        "-q",
-        "--quiet",
-        action="store_true",
-        help="suppress status messages",
-    )
-    parser.add_argument(
-        "file", type=argparse.FileType("r"), nargs="+", help="an XML file to validate"
-    )
-
-    args = parser.parse_args()
-    typemap = {}
-    if args.map:
-        try:
-            rawmap = json.load(args.map)
-        except (IOError, ValueError) as why:
-            sys.stderr.write(f"ERROR loading JSON: {why}\n")
+    def run(self):
+        errors = 0
+        for fh in self.args.file:
+            handler = RfcHttpValidator(self.typemap, self.status)
+            handler.filename = fh.name
+            sax.parse(fh, handler)
+            errors += handler.errors
+        if errors > 0:
             sys.exit(1)
-        try:
-            typemap.update(
-                {
-                    k.lower(): {
-                        "item": http_sfv.Item,
-                        "list": http_sfv.List,
-                        "dict": http_sfv.Dictionary,
-                    }[v]
-                    for (k, v) in rawmap.items()
-                }
-            )
-        except KeyError as why:
-            sys.stderr.write(f"ERROR loading JSON value: {why}\n")
-            sys.exit(1)
-    for item in args.item:
-        typemap[item.lower()] = http_sfv.Item
-    for _list in args.list:
-        typemap[_list.lower()] = http_sfv.List
-    for _dict in args.dict:
-        typemap[_dict.lower()] = http_sfv.Dictionary
-    if args.quiet:
-        global verbose
-        verbose = False
 
-    validate(args.file, typemap)
+    def parse_args(self):
+        parser = argparse.ArgumentParser(
+            description="Validate HTTP messages in XML2RFC documents"
+        )
+        parser.add_argument(
+            "-m",
+            "--map",
+            dest="map",
+            type=argparse.FileType("r"),
+            help="JSON file that maps field names to structured types",
+        )
+        parser.add_argument(
+            "-i",
+            "--item",
+            dest="item",
+            action="append",
+            default=[],
+            help="field name to consider as a Structured Item",
+        )
+        parser.add_argument(
+            "-l",
+            "--list",
+            dest="list",
+            action="append",
+            default=[],
+            help="field name to consider as a Structured List",
+        )
+        parser.add_argument(
+            "-d",
+            "--dict",
+            dest="dict",
+            action="append",
+            default=[],
+            help="field name to consider as a Structured Dictionary",
+        )
+        parser.add_argument(
+            "-q",
+            "--quiet",
+            dest="quiet",
+            action="store_true",
+            help="suppress status messages",
+        )
+        parser.add_argument(
+            "file",
+            type=argparse.FileType("r"),
+            nargs="+",
+            help="an XML file to validate",
+        )
+        return parser.parse_args()
+
+    def load_typemap(self):
+        typemap = {}
+        if self.args.map:
+            try:
+                rawmap = json.load(self.args.map)
+            except (IOError, ValueError) as why:
+                sys.stderr.write(f"ERROR loading JSON: {why}\n")
+                sys.exit(1)
+            try:
+                typemap.update(
+                    {
+                        k.lower(): {
+                            "item": http_sfv.Item,
+                            "list": http_sfv.List,
+                            "dict": http_sfv.Dictionary,
+                        }[v]
+                        for (k, v) in rawmap.items()
+                    }
+                )
+            except KeyError as why:
+                sys.stderr.write(f"ERROR loading JSON value: {why}\n")
+                sys.exit(1)
+        for item in self.args.item:
+            typemap[item.lower()] = http_sfv.Item
+        for _list in self.args.list:
+            typemap[_list.lower()] = http_sfv.List
+        for _dict in self.args.dict:
+            typemap[_dict.lower()] = http_sfv.Dictionary
+        return typemap
 
 
 if __name__ == "__main__":
-    main()
+    ValidatorCLI()
